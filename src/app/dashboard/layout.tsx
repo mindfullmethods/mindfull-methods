207a2b1b2ee1c5d3683d7f3ad434121178283b80
mindfullmethods <mindfullmethod@gmail.com>
2026-05-23 00:55:08 +0530

    Fixed Sidebar in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,29 +1,39 @@
 "use client";
 
 import { useEffect } from "react";
- import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
+
+import Sidebar from "@/components/components/dashboard/Sidebar";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
- }) {
+}) {
   const router = useRouter();
 
-   useEffect(() => {
-     async function checkUser() {
-       const {
+  useEffect(() => {
+    async function checkUser() {
+      const {
         data: { session },
       } = await supabase.auth.getSession();
 
       if (!session) {
         router.push("/login");
-       }
-     }
+      }
+    }
 
-     checkUser();
-   }, [router]);
+    checkUser();
+  }, [router]);
 
-  return <>{children}</>;
+  return (
+    <div className="flex">
+      <Sidebar />
+
+      <main className="flex-1">
+        {children}
+      </main>
+    </div>
+  );
 }
